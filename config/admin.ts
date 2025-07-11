0c6abde0b756e16909229f2eca4c25a80ad7746e
Jushoru <smirnov.kirill.kaduy@gmail.com>
2025-07-11 16:54:52 +0300

    edit: add dev settings for server.ts and admin.ts

--- config/admin.ts
+++ config/admin.ts
@@ -1,17 +1,21 @@
+import crypto from 'crypto';
+
+const generateKey = () => crypto.randomBytes(16).toString('hex');
+
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', generateKey()),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', generateKey()),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', generateKey()),
     },
   },
   secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
+    encryptionKey: env('ENCRYPTION_KEY', generateKey()),
   },
   flags: {
     nps: env.bool('FLAG_NPS', true),
